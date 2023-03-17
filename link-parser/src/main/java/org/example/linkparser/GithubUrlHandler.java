13f2b35d1d7749fd46e7801de971677eab9025a5
vladdan16 <v.danshov@innopolis.university>
2023-03-18 01:13:41 +0300

    rename constants

--- link-parser/src/main/java/org/example/linkparser/GithubUrlHandler.java
+++ link-parser/src/main/java/org/example/linkparser/GithubUrlHandler.java
@@ -10,20 +10,15 @@ public final class GithubUrlHandler extends UrlHandler {
     /**
      * Pattern for Github link
      */
-    private final Pattern githubUrlPattern;
+    private static final Pattern GITHUB_URL_PATTERN = Pattern.compile("^https://github\\.com/[^/]+/[^/]+/$");
 
-    /**
-     * Child constructor that set a pattern for link
-     * @param nextHandler Handler that will be called next
-     */
     public GithubUrlHandler(UrlHandler nextHandler) {
         super(nextHandler);
-        githubUrlPattern = Pattern.compile("^https://github\\.com/[^/]+/[^/]+/$");
     }
 
     @Override
     public ParseResult parseUrl(String url) {
-        Matcher matcher = githubUrlPattern.matcher(url);
+        Matcher matcher = GITHUB_URL_PATTERN.matcher(url);
         if (matcher.matches()) {
             String[] list = url.split("/");
             return new GithubParseResult(list[3], list[4]);
--- link-parser/src/main/java/org/example/linkparser/StackOverflowUrlHandler.java
+++ link-parser/src/main/java/org/example/linkparser/StackOverflowUrlHandler.java
@@ -10,20 +10,15 @@ public final class StackOverflowUrlHandler extends UrlHandler {
     /**
      * Pattern for StackOverflow link
      */
-    private final Pattern stackOverflowUrlPattern;
+    private static final Pattern STACK_OVERFLOW_URL_PATTERN = Pattern.compile("^https://stackoverflow\\.com/questions/\\d+/.+$");
 
-    /**
-     * Child constructor that set a pattern for link
-     * @param nextHandler Handler that will be called next
-     */
     public StackOverflowUrlHandler(UrlHandler nextHandler) {
         super(nextHandler);
-        stackOverflowUrlPattern = Pattern.compile("^https://stackoverflow\\.com/questions/\\d+/.+$");
     }
 
     @Override
     public ParseResult parseUrl(String url) {
-        Matcher matcher = stackOverflowUrlPattern.matcher(url);
+        Matcher matcher = STACK_OVERFLOW_URL_PATTERN.matcher(url);
         if (matcher.matches()) {
             String[] list = url.split("/");
             return new StackOverflowParseResult(list[4]);
