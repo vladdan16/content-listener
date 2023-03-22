4fd0543dc5109860ba26f542062d20196ec40b6c
Vladislav <v.danshov@innopolis.university>
2023-03-22 20:36:16 +0300

    task4

--- scrapper/src/main/java/org/example/scrapper/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/org/example/scrapper/configuration/ApplicationConfig.java
@@ -1,9 +1,17 @@
 package org.example.scrapper.configuration;
 
 import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
+import reactor.core.scheduler.Scheduler;
+
+import java.time.Duration;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
-public record ApplicationConfig(@NotNull String test) {}
+public record ApplicationConfig(@NotNull String test, Scheduler scheduler) {
+    @Value("${app.scheduler.interval}")
+    private static Duration schedulerInterval;
+
+}
