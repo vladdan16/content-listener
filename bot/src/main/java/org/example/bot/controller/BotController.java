160a05d4f4816bed28476bf8b9a5792a7db724d8
Vladislav <v.danshov@innopolis.university>
2023-03-29 13:10:44 +0300

    fixed handling exceptions
    Handling exceptions via RestControllerAdvice

--- bot/src/main/java/org/example/bot/controller/BotController.java
+++ bot/src/main/java/org/example/bot/controller/BotController.java
@@ -1,16 +1,10 @@
 package org.example.bot.controller;
 
-import org.example.bot.dto.ApiErrorResponse;
-import org.example.bot.dto.LinkUpdateResponse;
 import org.example.bot.dto.LinkUpdateRequest;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @RequestMapping("/updates")
@@ -21,19 +15,5 @@ public class BotController {
         // TODO: Process link update
 
         //return ResponseEntity.ok(new LinkUpdateResponse("Update processed"));
-    }
-
-    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    public ApiErrorResponse handleValidationException(MethodArgumentNotValidException e) {
-        List<String> errors = e.getBindingResult().getAllErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).toList();
-        return new ApiErrorResponse("Validation failed", "400", e.getClass().getSimpleName(), e.getMessage(), errors);
-    }
-
-    @ExceptionHandler(value = {Exception.class})
-    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-    public ApiErrorResponse handleException(Exception e) {
-        List<String> errors = List.of(Arrays.toString(e.getStackTrace()));
-        return new ApiErrorResponse("Internal server error", "500", e.getClass().getSimpleName(), e.getMessage(), errors);
     }
 }
--- scrapper/src/main/java/org/example/scrapper/controller/ScrapperController.java
+++ scrapper/src/main/java/org/example/scrapper/controller/ScrapperController.java
@@ -1,16 +1,11 @@
 package org.example.scrapper.controller;
 
-import org.example.scrapper.dto.*;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.HttpStatus;
+import org.example.scrapper.dto.AddLinkRequest;
+import org.example.scrapper.dto.LinkResponse;
+import org.example.scrapper.dto.ListLinksResponse;
+import org.example.scrapper.dto.RemoveLinkRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-
-import javax.management.AttributeNotFoundException;
-import javax.management.InstanceNotFoundException;
-import java.util.Arrays;
-import java.util.List;
 
 @RestControllerAdvice
 @RequestMapping("/tg-scrapper")
@@ -46,26 +41,5 @@ public class ScrapperController {
     public ResponseEntity<Void> removeLink(Long id, @RequestBody RemoveLinkRequest request) {
         // TODO: implement removing links
         return ResponseEntity.ok().build();
-    }
-
-    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    public ApiErrorResponse handleValidationException(MethodArgumentNotValidException e) {
-        List<String> errors = e.getBindingResult().getAllErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).toList();
-        return new ApiErrorResponse("Validation failed", "400", e.getClass().getSimpleName(), e.getMessage(), errors);
-    }
-
-    @ExceptionHandler(value = {InstanceNotFoundException.class, AttributeNotFoundException.class})
-    @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public ApiErrorResponse handleChatNotFoundException(InstanceNotFoundException e) {
-        List<String> errors = List.of(Arrays.toString(e.getStackTrace()));
-        return new ApiErrorResponse("Not found", "404", e.getClass().getSimpleName(), e.getMessage(), errors);
-    }
-
-    @ExceptionHandler(value = {Exception.class})
-    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-    public ApiErrorResponse handleException(Exception e) {
-        List<String> errors = List.of(Arrays.toString(e.getStackTrace()));
-        return new ApiErrorResponse("Internal server error", "500", e.getClass().getSimpleName(), e.getMessage(), errors);
     }
 }
