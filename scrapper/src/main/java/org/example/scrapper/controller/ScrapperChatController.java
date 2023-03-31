dbc280cafc3e33c63879193040a6752d0be4d334
Vladislav <v.danshov@innopolis.university>
2023-03-31 13:16:47 +0300

    tg-chat

--- scrapper/src/main/java/org/example/scrapper/controller/ScrapperChatController.java
+++ scrapper/src/main/java/org/example/scrapper/controller/ScrapperChatController.java
@@ -1,21 +1,19 @@
 package org.example.scrapper.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/tg-chat")
 public class ScrapperChatController {
 
-    @PostMapping("/tg-chat/{id}")
+    @PostMapping("/{id}")
     public ResponseEntity<Void> registerChat(@PathVariable Long id) {
         // TODO: register chat
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/tg-chat/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteChat(@PathVariable Long id) {
         // TODO: delete chat
         return ResponseEntity.ok().build();
