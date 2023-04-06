80cd16f8869e1548e5a0f517fb05291f11b0c368
Vladislav <v.danshov@innopolis.university>
2023-04-06 22:40:18 +0300

    comments

--- migration/db/changelog/create_database/create_chat_table.sql
+++ migration/db/changelog/create_database/create_chat_table.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
-
+--changeset create_database:1
 create table if not exists chat
 (
     id BIGINT primary key
--- migration/db/changelog/create_database/create_link_type_table.sql
+++ migration/db/changelog/create_database/create_link_type_table.sql
@@ -1,5 +1,6 @@
 --liquibase formatted sql
 
+--changeset create_database:1
 create table if not exists link_type
 (
     id   integer primary key,
