1f6562ab055daa742c3dddccc67fe05cf53989a8
Vladislav <v.danshov@innopolis.university>
2023-04-07 18:35:53 +0300

    update tables

--- migration/migrations/changelog/create_database/create_link_table.sql
+++ migration/migrations/changelog/create_database/create_link_table.sql
@@ -1,12 +1,13 @@
 --liquibase formatted sql
 
 --changeset create_database:1
+create sequence if not exists link_sequence start 1;
 create table if not exists link
 (
-    id       bigint primary key,
-    link     varchar(255) not null,
-    link_type_id integer not null,
-    owner_id bigint       not null,
+    id           bigint default nextval('link_sequence') primary key,
+    link         varchar(255) not null,
+    link_type_id integer      not null,
+    owner_id     bigint       not null,
     foreign key (owner_id) references chat (id),
     foreign key (link_type_id) references link_type (id)
 );
--- migration/migrations/changelog/create_database/create_link_type_table.sql
+++ migration/migrations/changelog/create_database/create_link_type_table.sql
@@ -1,8 +1,9 @@
 --liquibase formatted sql
 
 --changeset create_database:1
+create sequence if not exists link_type_sequence start 1;
 create table if not exists link_type
 (
-    id   integer primary key,
+    id   integer default nextval('link_type_sequence') primary key,
     type varchar(255) not null
 );
