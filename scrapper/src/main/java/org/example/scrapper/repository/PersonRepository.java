2bf2bd50b557cb2489715e0a5f9e4c20809fb8eb
Vladislav <v.danshov@innopolis.university>
2023-04-07 20:05:52 +0300

    fix mistakes

--- scrapper/src/main/java/org/example/scrapper/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.scrapper.repository;
-
-import org.example.scrapper.entity.Chat;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends JpaRepository<Chat, Long> {
-}
--- scrapper/src/main/java/org/example/scrapper/service/ScrapperService.java
+++
@@ -1,5 +0,0 @@
-package org.example.scrapper.service;
-
-public class ScrapperService {
-
-}
