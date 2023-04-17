75d81785cadbecdeeecdb7def17da25c513168b7
Vladislav <v.danshov@innopolis.university>
2023-04-17 15:47:03 +0300

    removed link type

--- migration/migrations/changelog/create_database/create_link_type_table.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
-
---changeset create_database:1
-create sequence if not exists link_type_sequence start 1;
-create table if not exists link_type
-(
-    id   integer default nextval('link_type_sequence') primary key,
-    type varchar(255) not null
-);
