0676e3c728f3811638fc748be242b86d9a17f6db
Vladislav <v.danshov@innopolis.university>
2023-04-19 01:06:53 +0300

    fixed test
    using @Mock and @InjectMocks annotations

--- bot/src/test/java/org/example/bot/core/commands/ListCommandTest.java
+++ bot/src/test/java/org/example/bot/core/commands/ListCommandTest.java
@@ -7,9 +7,11 @@ import com.pengrad.telegrambot.request.SendMessage;
 import org.example.bot.client.ScrapperClient;
 import org.example.bot.client.dto.LinkResponse;
 import org.example.bot.client.dto.ListLinksResponse;
-import org.example.bot.core.commands.ListCommand;
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -18,21 +20,31 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 public class ListCommandTest {
+    @InjectMocks
+    ListCommand listCommand;
+    @Mock
+    ScrapperClient scrapperClient;
+    @Mock
+    Chat chat;
+    @Mock
+    Message message;
+    @Mock
+    Update update;
+    @Before
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
     public void testHandleWithEmptyLinks() {
-        ScrapperClient scrapperClient = Mockito.mock(ScrapperClient.class);
-        ListCommand listCommand = new ListCommand(scrapperClient);
         long chatId = 123L;
-        Chat chat = Mockito.mock(Chat.class);
-        Mockito.when(chat.id()).thenReturn(chatId);
-        Message message = Mockito.mock(Message.class);
-        Mockito.when(message.chat()).thenReturn(chat);
-        Update update = Mockito.mock(Update.class);
-        Mockito.when(update.message()).thenReturn(message);
-        Mockito.when(scrapperClient.getLinks(chatId)).thenReturn(new ListLinksResponse(Collections.emptyList(), 0));
+        when(chat.id()).thenReturn(chatId);
+        when(message.chat()).thenReturn(chat);
+        when(update.message()).thenReturn(message);
+        when(scrapperClient.getLinks(chatId)).thenReturn(new ListLinksResponse(Collections.emptyList(), 0));
 
         SendMessage result = listCommand.handle(update);
         SendMessage expected = new SendMessage(chatId, "There are no links to track");
@@ -43,19 +55,14 @@ public class ListCommandTest {
 
     @Test
     public void testHandleWithLinks() throws URISyntaxException {
-        ScrapperClient scrapperClient = Mockito.mock(ScrapperClient.class);
-        ListCommand listCommand = new ListCommand(scrapperClient);
         long chatId = 123L;
-        Chat chat = Mockito.mock(Chat.class);
-        Mockito.when(chat.id()).thenReturn(chatId);
-        Message message = Mockito.mock(Message.class);
-        Mockito.when(message.chat()).thenReturn(chat);
-        Update update = Mockito.mock(Update.class);
-        Mockito.when(update.message()). thenReturn(message);
+        when(chat.id()).thenReturn(chatId);
+        when(message.chat()).thenReturn(chat);
+        when(update.message()). thenReturn(message);
         List<LinkResponse> links = new ArrayList<>();
         links.add(new LinkResponse(1L, new URI("https://github.com/vladdan16/content-listener")));
         links.add(new LinkResponse(2L, new URI("https://stackoverflow.com/questions/1642028/what-is-the-operator-in-c")));
-        Mockito.when(scrapperClient.getLinks(chatId)).thenReturn(new ListLinksResponse(links, links.size()));
+        when(scrapperClient.getLinks(chatId)).thenReturn(new ListLinksResponse(links, links.size()));
 
         SendMessage result = listCommand.handle(update);
         SendMessage expected = new SendMessage(chatId, """
