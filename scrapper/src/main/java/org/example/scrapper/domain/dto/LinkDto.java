a1b6c12254b9a78bed2f102bd0e32baddbf7d92b
Vladislav <v.danshov@innopolis.university>
2023-04-19 13:26:24 +0300

    entity

--- scrapper/src/main/java/org/example/scrapper/domain/dto/LinkDto.java
+++ scrapper/src/main/java/org/example/scrapper/domain/dto/LinkDto.java
@@ -4,9 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Data
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 public class LinkDto {
