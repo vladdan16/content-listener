ce5518f8610d0db0a9491ebce3ae3fc20c658177
Vladislav <v.danshov@innopolis.university>
2023-04-20 17:31:55 +0300

    Update create_chat_link_table.sql

--- migration/migrations/changelog/create_database/create_chat_link_table.sql
+++ migration/migrations/changelog/create_database/create_chat_link_table.sql
@@ -3,9 +3,7 @@
 --changeset create_database:1
 create table if not exists chat_link
 (
-    chat_id bigint,
-    link_id bigint,
-    primary key (chat_id, link_id),
-    foreign key (chat_id) references chat (id),
-    foreign key (link_id) references link (id)
+    chat_id bigint references chat (id),
+    link_id bigint references link (id),
+    primary key (chat_id, link_id)
 );
