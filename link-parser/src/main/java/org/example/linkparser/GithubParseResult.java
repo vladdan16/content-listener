79d7b3a3eae02d7e1a5218fecc436b65d9e84889
Vladislav <v.danshov@innopolis.university>
2023-05-06 01:58:02 +0300

    checkstyle scrapper 2

--- link-parser/src/main/java/org/example/linkparser/GithubParseResult.java
+++ link-parser/src/main/java/org/example/linkparser/GithubParseResult.java
@@ -1,21 +1,16 @@
 package org.example.linkparser;
 
-import org.jetbrains.annotations.Contract;
-import org.jetbrains.annotations.NotNull;
-
 /**
  * Record for result of parsing GitHub link.
  * @param user Name of user account on GitHub
  * @param repo Repository name
  */
 public record GithubParseResult(String user, String repo) implements ParseResult {
-    @Contract(pure = true) @Override
-    public @NotNull String toString() {
+    public String toString() {
         return user + "/" + repo;
     }
 
-    @Contract(pure = true) @Override
-    public @NotNull String getLinkType() {
+    public String getLinkType() {
         return "github";
     }
 }
